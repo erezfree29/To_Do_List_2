65bf09a6c651f0affba1cd0f9cd18d4f5a6b5f9f
Erez <erezfree29@gmail.com>
2021-06-13 15:00:01 -0700

    inital configuration

---
+++ index.js
@@ -0,0 +1,5 @@
+import moment from 'moment';
+console.log("Hello from JavaScript!");
+console.log(moment().startOf('day').fromNow());
+var name = "Bob", time = "today";  
+console.log(`Hello ${name}, how are you ${time}?`);
