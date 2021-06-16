31522415d592212f2d70c91211a304dc2914fd08
Erez <erezfree29@gmail.com>
2021-06-16 11:42:25 -0700

    add menu page innerHtml

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -64,7 +64,10 @@ contactButton.onclick = function () {
 </div>
   `;
   container.appendChild(usheader);
-  document.body.appendChild(contactcontent);
+  const searchContent = document.querySelector('.contact_content');
+  if (searchContent === null) {
+    document.body.appendChild(contactcontent);
+  }
 };
 
 menuButton.onclick = function () {
@@ -82,15 +85,78 @@ menuButton.onclick = function () {
   container.classList.add('container');
   container.classList.add('menu_container');
   menuContent.appendChild(container);
-  container.innerHTML = `
-  <div class="our_menu"></div>
-    <h2>Our Menu</h2>
-    <div class="contactp dish_one">
-    <div class="row">Dish: Mashroms pasta</div>
-    <div class="row">cousine: Italian</div>
-    <div class="row">Price: $18</div>
-  </div> 
- </div>
+  const desert = document.createElement('h3');
+  desert.classList.add('desert');
+  desert.textContent = 'Desert';
+  const deserts = document.createElement('div');
+  deserts.classList.add('deserts');
+  deserts.innerHTML = `
+  <div class="dish">
+       <div>treat: Choclate Brownie</div>
+         <div>cousine: American</div>
+        <div>Price: $6</div>
+      </div>
+       <div class="dish">
+         <div>Treat: Gelate Ice Cream</div>
+        <div>cousine: Italian</div>
+        <div>Price: $5</div>
+     </div>
+      <div class="dish">
+       <div>Treat: Carrot Cake</div>
+         <div>cousine: British/div>
+      <div>Price: $4</div>
+       </div>   
   `;
-  document.body.appendChild(menuContent);
+
+  container.innerHTML = `
+  <div class="our_menu_header">
+    <h2>Our Menu</h2>
+  </div>
+  <div class="new_menu">
+    <div class="starters">
+      <h3>Starters</h3>
+      <div>
+          <div>Dish: Greek olives</div>
+          <div>cousine: Greek</div>
+          <div>Price: $4</div>
+      </div>
+      <div class="dish">
+        <div>Dish: Hot potatoes</div>
+        <div>cousine: Irish</div>
+        <div>Price: $4</div>
+      </div>
+      <div class="dish">
+        <div>Dish: fuul</div>
+        <div>cousine: Egyptian</div>
+        <div>Price: $6</div>
+      </div>  
+    </div>
+    <div>
+      <h3>Mains</h3>
+      <div class="dish">
+        <div>Dish: Mashroms pasta/div>
+          <div>cousine: Italian</div>
+          <div>Price: $12</div>
+        </div>
+        <div class="dish">
+          <div>Dish: Rice Curry</div>
+          <div>cousine: Indian</div>
+          <div>Price: $9</div>
+        </div>
+        <div class="dish">
+          <div>Dish: Sushi</div>
+          <div>cousine: Japanes</div>
+        <div>Price: $13</div>
+      </div> 
+    </div>
+  </div>
+  `;
+  container.appendChild(desert);
+  container.appendChild(deserts);
+  const searchMenuContent = document.querySelector('.menu_content');
+  if (searchMenuContent === null) {
+    document.body.appendChild(menuContent);
+  }
 };
+
+//
