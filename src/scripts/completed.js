131cb635b999c18e46afe2bf02a5aeecf6604702
Erez <erezfree29@gmail.com>
2021-07-13 21:29:02 -0700

    drag and drop element

---
+++ src/scripts/completed.js
@@ -0,0 +1,45 @@
+const completed = (() => {
+  const taskOne = {
+    description: 'Clean car',
+    completed: true,
+    index: 0,
+  };
+  const taskTwo = {
+    description: 'Do insurance',
+    completed: false,
+    index: 1,
+  };
+  const taskThree = {
+    description: 'Deposit money',
+    completed: false,
+    index: 2,
+  };
+  const tasksArray = [];
+  tasksArray.push(taskOne);
+  tasksArray.push(taskTwo);
+  tasksArray.push(taskThree);
+  tasksArray.forEach((task) => {
+    const toDoList = document.querySelector('.lists_container');
+    const list = document.querySelector('#columns');
+    const liDescription = document.createElement('li');
+    liDescription.classList.add('column');
+    liDescription.setAttribute('draggable', true);
+    liDescription.innerHTML = '<input type="checkbox">';
+    liDescription.firstChild.id = task.description;
+    liDescription.addEventListener('change', () => {
+      if (task.completed === false) {
+        task.completed = true;
+      } else {
+        task.completed = false;
+      }
+      localStorage.setItem('tasksListArray', JSON.stringify(tasksArray));
+    });
+
+    liDescription.innerHTML += task.description;
+    list.appendChild(liDescription);
+    toDoList.appendChild(list);
+  });
+  localStorage.setItem('tasksListArray', JSON.stringify(tasksArray));
+})();
+
+export default completed;
